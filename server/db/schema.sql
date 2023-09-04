d4c2f121359f099a09b6a88982720b5a65713618
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-09-04 14:33:29 -0600

    added foregin key constraints to schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS voyages_db;
 CREATE DATABASE voyages_db;
 USE voyages_db;
 
-CREATE TABLE users
+CREATE TABLE Users
 (
     userId INT NOT NULL
     AUTO_INCREMENT PRIMARY KEY,
@@ -14,32 +14,30 @@ CREATE TABLE users
     (30) NOT NULL
 );
 
-    ALTER TABLE users AUTO_INCREMENT = 1000;
+    ALTER TABLE Users AUTO_INCREMENT = 1000;
 
-    CREATE TABLE voyages
+    CREATE TABLE Voyages
     (
-        userId INT NOT NULL,
+        uId INT NOT NULL,
         voyageId INT NOT NULL
         AUTO_INCREMENT PRIMARY KEY,
         voyageName VARCHAR
-        (30) NOT NULL
-        -- FOREIGN KEY
-        -- (userId) REFERENCES users
-        -- (userId)
-        -- ON
-        -- DELETE CASCADE
+        (30) NOT NULL,
+        FOREIGN KEY
+        (uId) REFERENCES Users
+        (userId) ON
+        DELETE CASCADE
     );
 
-        ALTER TABLE voyages AUTO_INCREMENT = 1000000;
+        ALTER TABLE Voyages AUTO_INCREMENT = 1000000;
 
-        CREATE TABLE log
+        CREATE TABLE Log
         (
             voyageId INT NOT NULL PRIMARY KEY,
             logDate DATE NOT NULL,
             latitude VARCHAR(20) NOT NULL,
             longitude VARCHAR(20) NOT NULL,
             heading INT,
-            notes TEXT
-            -- FOREIGN KEY (voyageId) REFERENCES voyages(voyageId)
-            -- ON DELETE CASCADE
+            notes TEXT,
+            FOREIGN KEY (voyageId) REFERENCES Voyages(voyageId) ON DELETE CASCADE
         );
--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -1,20 +1,20 @@
-INSERT INTO users
+INSERT INTO Users
     (userName, userEmail, userPassword)
 VALUES
     ("CaptAndrew", "andrew@test.com", "test1234"),
     ("CaptJim", "jim@test.com", "test1234"),
     ("CaptHaddy", "haddy@test.com", "test1234");
 
-INSERT INTO voyages
-    (userId, voyageName)
+INSERT INTO Voyages
+    (uId, voyageName)
 VALUES
     (1000, "Catalina"),
-    (1002, "Anacapa Island"),
-    (1003, "Santa Cruz Island");
+    (1001, "Anacapa Island"),
+    (1002, "Santa Cruz Island");
 
-INSERT INTO log
+INSERT INTO Log
     (voyageId, logDate, latitude, longitude, heading, notes)
 VALUES
     (1000000, "2023-09-01", "33° 43.3987 N", "118° 11.0188 W", 232, "Fair Winds!"),
-    (1000002, "2023-09-01", "33° 26.9491 N", "118° 29.6037 W", 333, "Fair Winds!"),
-    (1000003, "2023-09-01", "33° 27.7843 N", "118° 29.8885 W", 305, "Fair Winds!");
+    (1000001, "2023-09-01", "33° 26.9491 N", "118° 29.6037 W", 333, "Fair Winds!"),
+    (1000002, "2023-09-01", "33° 27.7843 N", "118° 29.8885 W", 305, "Fair Winds!");
