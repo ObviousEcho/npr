db924f58a7b783c5751eec85af41bde219cceb48
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-09-07 12:13:02 -0600

    npm jwt, created signToken in utils/auth, tokens signed and returned to client upon signup/login

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-const { validate, hashed } = require("../utils/helpers");
+const { validate } = require("../utils/helpers");
+const { signToken } = require("../utils/auth");
 const db = require("../config/connections").databaseConnection;
 
+// get all users
 router.get("/", (req, res) => {
   const sql = `SELECT * FROM Users`;
 
@@ -20,6 +22,7 @@ router.get("/", (req, res) => {
   });
 });
 
+// create new user
 router.post("/signup", async ({ body }, res) => {
   // validator helper function
   if (!validate(body.userEmail, body.userPassword)) {
@@ -27,23 +30,38 @@ router.post("/signup", async ({ body }, res) => {
     return;
   }
 
+  const userName = body.userName;
+  const userEmail = body.userEmail;
+
   // hash password using bcrypt
   const hashedPassword = await bcrypt.hash(body.userPassword, 10);
 
   const sql = `INSERT INTO Users (userName, userEmail, userPassword) VALUES (?, ?, ?)`;
-  const params = [body.userName, body.userEmail, hashedPassword];
+  const params = [userName, userEmail, hashedPassword];
 
   db.query(sql, params, (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
     }
+
+    // compile jwt payload
+    const userId = rows.insertId;
+    const profile = {
+      userId: userId,
+      userName: userName,
+      userEmail: userEmail,
+    };
+    // sign jwt
+    const token = signToken(profile);
     res.json({
       message: "success",
+      token: token,
     });
   });
 });
 
+// login existing user
 router.post("/login", ({ body }, res) => {
   const sql = `SELECT userId, userName, userEmail, userPassword FROM Users WHERE userEmail = ?`;
   const params = [body.userEmail];
@@ -61,19 +79,25 @@ router.post("/login", ({ body }, res) => {
     );
 
     if (validPassword) {
-      // sign JWT
+      // compile jwt payload
+      const userId = rows[0].userId;
+      const userName = rows[0].userName;
+      const userEmail = rows[0].userEmail;
+      const profile = {
+        userId: userId,
+        userName: userName,
+        userEmail: userEmail,
+      };
+      // sign jwt
+      const token = signToken(profile);
       res.json({
         message: "success",
-        // data: JWT,
+        token: token,
       });
     } else {
       res.json({ message: "something went wrong!" });
     }
   });
-});
-
-router.post("/logout", (req, res) => {
-  // remove JWT
 });
 
 module.exports = router;
