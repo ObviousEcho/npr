91de5a8cce7d17cb69756f74a3a39d76bed78696
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-09-19 18:00:07 -0600

    added images to public folder, created Home route with Picture element

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import classes from "./Home.module.css";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <picture>
+      <source media="(min-width: 1920px" srcSet="/images/yacht-1920w.jpg" />
+      <source
+        media="(orientation: landscape) and (min-width: 1000px)"
+        srcSet="/images/yacht-1366x1024.jpg"
+      />
+      <source
+        media="(orientation: portrait) and (min-width: 750px)"
+        srcSet="/images/yacht-1024x1366.jpg"
+      />
+      <source
+        media="(orientation: landscape) and (min-width: 420px)"
+        srcSet="/images/yacht-896x414.jpg"
+      />
+      <img
+        src="/images/yacht-450x915.jpg"
+        alt="Yacht anchored in turquoise colored ocean."
+        className={classes.image}
+      />
+    </picture>
+  );
 };
 
 export default Home;
